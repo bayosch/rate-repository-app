e02bb0fd81f73361966f175be81bc610dae075c4
Byron Chicaiza <bayosch@gmail.com>
2023-05-08 10:08:13 -0500

    First push

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { FlatList, Text } from 'react-native';
-import useRepositories from '../hooks/useRepositories.js';
 import useFirestoreRepositories from '../hooks/useFirestoreRepositories.js';
 import RepositoryItem from './RepositoryItem.jsx';
 
 const RepositoryList = () => {
     const { repositories } = useFirestoreRepositories();
-    //const { repositories } = useRepositories();
     return (
         <FlatList data={repositories}
             ItemSeparatorComponent={() => <Text></Text>}
